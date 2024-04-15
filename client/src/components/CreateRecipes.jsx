321743a1800cdaa8207ca30b8c5df72dd861a4cc
CRISPY227 <daedalus227@gmail.com>
2024-04-15 11:59:27 -0700

    Create recipie page
    CURRY RICE

--- client/src/components/CreateRecipes.jsx
+++ client/src/components/CreateRecipes.jsx
@@ -1,25 +1,58 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from 'axios'
+import { useNavigate } from "react-router-dom";
 
-const CreateRecipes = () => {
-  return (
-    <div>
-        <section id="createrecipes">
-            <div className="container my-5 py-5">
-                <div className="row">
-                    <div className="col-md-6">
-                        <img src="/assets/about1.png" alt="createrecipes"
-                        className="w-75 mt-5" />
+//Function that Grabs each Event (Input data) and sets each input into a useState where our data will be stored for creating new Recipes
+function CreateRecipes  () {
+    const [title, setTitle] = useState()
+    const [description, setDescription] = useState()
+    const [ingredients, setIngredients] = useState()
+    const [tags, setTags] = useState()
+    const navigate = useNavigate()
+
+    //Once the User clicks submit each Event (e) gets posted to the database and get's loged whether it succesfully succeded for failed.
+    const Submit = (e) => {
+        e.preventDefault();
+        axios.post("http://localhost:3001/createRecipe", {title, description, ingredients, tags})
+        .then(result => {
+            console.log(result)
+            navigate('/')
+        })
+        .catch(err => console.log(err))
+    }
+
+
+    return (
+        //Page Elements and Structure
+        <div className = "d-flex vh-100 bg-primary justify-content-center align-items-center">
+            <div className='w-50 bg-white rounded p-3'>
+                <form onSubmit={Submit}>
+                    <h2>Add Recipe</h2>
+                    <div className='mb-2'>
+                        <label htmlFor="">Title</label>
+                        <input type="text" placeholder='Enter Recipe Name' className='form-control' 
+                        onChange={(e) => setTitle(e.target.value)}/>
                     </div>
-                    <div className="col-md-6">
-                        <h1 className="display-6">THIS IS THE ADD PAGE</h1>
-                        <hr className="w-50"/>
-                        <p className="lead mb-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae tempora doloribus alias dolorum illo quis vel laborum amet! Adipisci, obcaecati asperiores omnis atque sunt vero reprehenderit quae fuga facere amet id. Blanditiis porro nam eaque reprehenderit voluptates rem consequatur. Nihil eius mollitia aspernatur temporibus voluptatibus dolorem iste. Voluptatem, ut cumque molestias, laboriosam aspernatur quas ipsa, unde totam ea fuga exercitationem sapiente inventore minus. Optio ad molestiae unde labore quas est odit? Ullam sapiente aliquam mollitia fuga sed eveniet, iste placeat.</p>
+                    <div className='mb-2'>
+                        <label htmlFor="">Description</label>
+                        <input type="text" placeholder='Enter Recipe Description' className='form-control' 
+                        onChange={(e) => setDescription(e.target.value)}/>
                     </div>
-                </div>
+                    <div className='mb-2'>
+                        <label htmlFor="">Ingredients</label>
+                        <input type="text" placeholder='Enter Recipe Ingredients' className='form-control' 
+                        onChange={(e) => setIngredients(e.target.value)}/>
+                    </div>
+                    <div className='mb-2'>
+                        <label htmlFor="">Tags</label>
+                        <input type="text" placeholder='Enter Recipe Tags' className='form-control' 
+                        onChange={(e) => setTags(e.target.value)}/>
+                    </div>
+                    <button className='btn btn-success'>Submit</button>
+                </form>
             </div>
-        </section>
-    </div>
-  );
+        </div>
+    )
 }
 
 export default CreateRecipes;
