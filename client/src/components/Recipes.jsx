b1485a1f8414a08d1f9ce605f04db9a197b4719d
CodetoHeaven <149557342+CodetoHeaven@users.noreply.github.com>
2024-04-17 10:43:36 -0700

    Update Recipes.jsx

--- client/src/components/Recipes.jsx
+++ client/src/components/Recipes.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import background from './kitchen.jpg';
 import { NavLink } from 'react-router-dom';
 import axios from 'axios';
+import {FaPizzaSlice, FaHamburger} from 'react-icons/fa';
+import {GiNoodles, GiChopsticks} from 'react-icons/gi';
+import styled from 'styled-components';
 
 const Recipes = () => {
     const [searchTerm, setSearchTerm] = useState('');
@@ -24,6 +27,25 @@ const Recipes = () => {
                         <div className="col-md-8 mt-5">
                             <h1 className="display-4 fw-bolder mb text-center">Recipe Search</h1>
                             <p className="lead text-center fs-4 mb-5">Let's Cook!</p>
+                            <List>
+                                <SLink to={'/cuisine/Italian'}>
+                                    <FaPizzaSlice />
+                                    <h4>Italian</h4>
+                                </SLink>
+                                <SLink to={'/cuisine/American'}>
+                                    <FaHamburger />
+                                    <h4>American</h4>
+                                </SLink>
+                                    <SLink to={'/cuisine/Thai'}>
+                                    <GiNoodles />
+                                    <h4>Thai</h4>
+                                </SLink>
+                                    <SLink to={'/cuisine/Japanese'}>
+                                    <GiChopsticks />
+                                    <h4>Japanese</h4>
+                                </SLink>
+
+                            </List>
                             <div className="input-group mb-3">
                                 <input 
                                     type="text"
@@ -59,5 +81,54 @@ const Recipes = () => {
         </div>
     );
 }
+
+
+const List = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: 2rem 0rem;
+    margin-left: 2rem;
+    
+`;
+
+const SLink = styled(NavLink)`
+    display: flex;
+    flex-direction: column;
+    justify-content:  center;
+    align-items: center;
+    border-radius: 50%;
+    margin-right: 2rem;
+    text-decoration: none;
+    background: linear-gradient(35deg, #494949, #313131);
+    width: 6rem;
+    height: 6rem;
+    cursor: pointer;
+    transform: scale(0.8);
+
+    h4{
+        color: white;
+        font-size: 0.8rem;
+
+    }
+    svg{
+        color: white;
+        font-size: 1.5rem;
+    }
+    &.active{
+        background: linear-gradient(to right, #f27121, #e94057);
+
+        svg{
+            color: white;
+
+        }
+        h4{
+            color: white;
+        }
+
+    }
+
+
+`;
+
 
 export default Recipes;
